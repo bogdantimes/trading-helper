b583867343df7b70a1aa6cfacc0b5c2922c2e485
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-01-20 00:02:33 +0200

    Testing

--- Main.ts
+++ Main.ts
@@ -1,9 +1,10 @@
 function doGet(e) {
-  return ContentService.createTextOutput(`handled doGet: ${getOrders("ETHUSDT")}`);
+  return ContentService.createTextOutput(`handled doGet: ${getFreeAsset("ETHUSDT")}`);
 }
 
 function doPost(e) {
-  return ContentService.createTextOutput('handled doPost');
+  console.log(JSON.stringify(e))
+  return ContentService.createTextOutput(`handled doPost: ${JSON.stringify(e)}`);
 }
 
 const API = "https://api.binance.com/api/v3";
@@ -11,20 +12,100 @@ const KEY = PropertiesService.getScriptProperties().getProperty('KEY')
 const SECRET = PropertiesService.getScriptProperties().getProperty('SECRET')
 
 function getOrders(symbol: string) {
-  const requestManager = new RequestManager(API);
-  requestManager.setRequestAttempts(2)
+  const resource = "allOrders";
+  const query = `symbol=${symbol}`;
+  const data = execute({
+    context: null,
+    interval: 500,
+    attempts: 60,
+    runnable: ctx => UrlFetchApp.fetch(`${API}/${resource}?${addSignature(query)}`, {headers: {'X-MBX-APIKEY': KEY}})
+  });
+  console.log(data.getContentText())
+  return data.getContentText()
+}
 
+function getFreeAsset(asset: string) {
+  const resource = "account"
+  const query = "";
+  const data = execute({
+    context: null,
+    interval: 500,
+    attempts: 60,
+    runnable: ctx => UrlFetchApp.fetch(`${API}/${resource}?${addSignature(query)}`, {headers: {'X-MBX-APIKEY': KEY}})
+  });
+  try {
+    const account = JSON.parse(data.getContentText());
+    const assetVal = account.balances.find((balance) => balance.asset == asset);
+    console.log(assetVal)
+    return assetVal ? assetVal.free : ""
+  } catch (e) {
+    console.log(e)
+  }
+  return ""
+}
+
+function marketBuy(symbol: string, side: string) {
+  const freeAsset = getFreeAsset(side == "BUY" ? "USDT" : symbol)
+
+  if (side == "BUY" && (!freeAsset || (+freeAsset < 60))) {
+    console.log(`NOT ENOUGH TO BUY, EXITING: USDT=${freeAsset}`)
+    return
+  }
+  if (!freeAsset || (+freeAsset < 1)) {
+    console.log(`NOT ENOUGH TO SELL, EXITING: ${symbol}=${freeAsset}`)
+    return
+  }
+
+  const resource = "order"
+  const query = `symbol=${symbol}&type=MARKET&side=BUY&quoteOrderQty=50`;
+  const data = execute({
+    context: null,
+    interval: 500,
+    attempts: 60,
+    runnable: ctx => UrlFetchApp.fetch(`${API}/${resource}?${addSignature(query)}`, {headers: {'X-MBX-APIKEY': KEY}})
+  });
+  console.log(data.getContentText())
+  return data.getContentText()
+}
+
+function marketSell(symbol: string) {
+  const freeAsset = getFreeAsset(symbol)
+  if (!freeAsset) {
+    return
+  }
+
+  const resource = "order"
+  const query = `symbol=${symbol}&type=MARKET&side=SELL&quantity=${freeAsset}`;
+  const data = execute({
+    context: null,
+    interval: 500,
+    attempts: 60,
+    runnable: ctx => UrlFetchApp.fetch(`${API}/${resource}?${addSignature(query)}`, {headers: {'X-MBX-APIKEY': KEY}})
+  });
+  console.log(data.getContentText())
+  return data.getContentText()
+}
+
+function getPrice(symbol: string) {
+  const resource = "ticker/price"
+  const query = `symbol=${symbol}`;
+  const data = execute({
+    context: null,
+    interval: 500,
+    attempts: 20,
+    runnable: ctx => UrlFetchApp.fetch(`${API}/${resource}?${query}`, {headers: {'X-MBX-APIKEY': KEY}})
+  });
+  console.log(data.getContentText())
+  return data.getContentText()
+}
+
+function addSignature(data: string) {
   const timestamp = Number(new Date().getTime()).toFixed(0);
-  const params = `timestamp=${timestamp}`;
-  const signature = Utilities.computeHmacSha256Signature(params, SECRET).map(e => {
+  const sigData = `${data}${data ? "&" : ""}timestamp=${timestamp}`
+  const sig = Utilities.computeHmacSha256Signature(sigData, SECRET).map(e => {
     const v = (e < 0 ? e + 256 : e).toString(16);
     return v.length == 1 ? "0" + v : v;
-  }).join("");
+  }).join("")
 
-  const options = requestManager.addHeader({muteHttpExceptions: true}, 'X-MBX-APIKEY', KEY)
-  const allOrders = requestManager.get("/openOrders", options, {timestamp, signature});
-
-  Logger.log(allOrders)
-
-  return allOrders
+  return `${sigData}&signature=${sig}`
 }
