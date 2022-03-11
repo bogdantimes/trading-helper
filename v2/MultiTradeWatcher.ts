8eae1ce5c447c47d545045cb4ca99472b460015a
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-03-11 11:44:56 +0000

    Cache all prices

--- v2/MultiTradeWatcher.ts
+++ v2/MultiTradeWatcher.ts
@@ -1,21 +1,19 @@
-const CHECK_ALL = 'CHECK_ALL';
-
 class MultiTradeWatcher {
   static start() {
     try {
-      ScriptApp.newTrigger(CHECK_ALL).timeBased().everyMinutes(1).create()
-      Log.info(`Started ${CHECK_ALL}`)
+      ScriptApp.newTrigger(CHECK_ALL.name).timeBased().everyMinutes(1).create()
+      Log.info(`Started ${CHECK_ALL.name}`)
     } catch (e) {
       Log.error(e)
     }
   }
 
   static stop() {
-    const trigger = ScriptApp.getProjectTriggers().find(t => t.getHandlerFunction() == CHECK_ALL);
+    const trigger = ScriptApp.getProjectTriggers().find(t => t.getHandlerFunction() == CHECK_ALL.name);
     if (trigger) {
       try {
         ScriptApp.deleteTrigger(trigger);
-        Log.info(`Stopped ${CHECK_ALL}`)
+        Log.info(`Stopped ${CHECK_ALL.name}`)
       } catch (e) {
         Log.error(e)
       }
@@ -46,32 +44,30 @@ class MultiTradeWatcher {
   }
 }
 
-function init(_runtimeCtx) {
-  _runtimeCtx[CHECK_ALL] = function () {
-    const store = DefaultStore;
-    const statistics = new Statistics(store);
-    const trader = new V2Trader(store, new Binance(store), statistics);
-    let sendLog = true;
+function CHECK_ALL() {
+  const store = DefaultStore;
+  const statistics = new Statistics(store);
+  const trader = new V2Trader(store, new Binance(store), statistics);
+  let sendLog = true;
 
-    Object.values(DefaultStore.getOrSet("trade", {}))
-      .forEach((tradeRaw: object) => {
-        const tradeMemo: TradeMemo = TradeMemo.fromObject(tradeRaw);
-        try {
-          const result = trader.stopLossSell(tradeMemo.tradeResult.symbol);
-          Log.info(result.toString())
-          sendLog = result.fromExchange
-          statistics.addProfit(result.profit)
-          statistics.addCommission(result.commission)
-        } catch (e) {
-          Log.error(e)
-        }
-      })
+  Object.values(DefaultStore.getOrSet("trade", {}))
+    .forEach((tradeRaw: object) => {
+      const tradeMemo: TradeMemo = TradeMemo.fromObject(tradeRaw);
+      try {
+        const result = trader.stopLossSell(tradeMemo.tradeResult.symbol);
+        Log.info(result.toString())
+        sendLog = result.fromExchange
+        statistics.addProfit(result.profit)
+        statistics.addCommission(result.commission)
+      } catch (e) {
+        Log.error(e)
+      }
+    })
 
-    if (sendLog) {
-      Log.ifUsefulDumpAsEmail()
-    }
+  if (sendLog) {
+    Log.ifUsefulDumpAsEmail()
   }
-}(this)
+}
 
 function Start() {
   MultiTradeWatcher.stop()
