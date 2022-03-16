93e58f6067065de373fce0ba371d74ca9af12193
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-03-16 18:00:50 +0100

    Add sell now button confirmation

--- v2/Visualizer.ts
+++ v2/Visualizer.ts
@@ -22,6 +22,16 @@ class V2TradeVisualizer implements TradeVisualizer {
 
     const profit = statistics.getTotalProfit().toFixed(2);
     const commission = (statistics.getTotalCommission() * this.bnbPrice).toFixed(2);
+    htmlOutput.append(`
+        <script>
+          function confirmAndSell(asset, button) {
+            if (confirm(\`Do you confirm selling \${asset}?\`)) {
+                button.disabled = true
+                google.script.run.quickSell(asset)
+            }
+          }
+        </script>
+`);
     htmlOutput.append(`<h1 style="color: ${+profit >= 0 ? 'forestgreen' : 'orangered'}">Total profit: ${profit} ${USDT}</h1>`);
     htmlOutput.append(`<h3 style="color: darkorange">Total commission: ~ ${commission} ${USDT}</h3>`);
 
@@ -44,12 +54,14 @@ class V2TradeVisualizer implements TradeVisualizer {
 
         const textStyle = Charts.newTextStyle().setColor('#e7e7e7').build();
 
-        const maxLoss = tradeMemo.maxLoss.toFixed(2);
-        const maxProfit = tradeMemo.maxProfit.toFixed(2);
+        const loss = tradeMemo.maxLoss.toFixed(2);
+        const profit = tradeMemo.maxProfit.toFixed(2);
+        const lossPercent = (100 * (tradeMemo.maxLoss / tradeMemo.tradeResult.paid)).toFixed(2)
+        const profitPercent = (100 * (tradeMemo.maxProfit / tradeMemo.tradeResult.paid)).toFixed(2)
         const chart = Charts.newLineChart()
           .setDataTable(data)
           .setYAxisTitle(`Price (${USDT})`)
-          .setXAxisTitle(`Loss estimate: ${maxLoss} ${USDT}\nProfit estimate: ${maxProfit} ${USDT}`)
+          .setXAxisTitle(`Profit estimate (${USDT}): ${profit} (${profitPercent} %)\nLoss estimate (${USDT}): ${loss} (${lossPercent} %)`)
           .setBackgroundColor('#081f21')
           .setTitleTextStyle(textStyle)
           .setLegendTextStyle(textStyle)
@@ -64,11 +76,11 @@ class V2TradeVisualizer implements TradeVisualizer {
           .build();
 
         const imageData = Utilities.base64Encode(chart.getAs('image/png').getBytes());
-        htmlOutput.append(`<div>
+        htmlOutput.append(`
             <img src="data:image/png;base64,${encodeURI(imageData)}">
-            <input type="button" value="SELL NOW" 
-                onclick="google.script.run.quickSell('${tradeMemo.getKey().symbol.quantityAsset}')" />
-        </div>`)
+            <input type="button" value="SELL NOW" style="position: relative; left: -93px; top: -10px"
+                onclick="confirmAndSell('${tradeMemo.getKey().symbol.quantityAsset}', this)" />
+        `)
       })
 
     return htmlOutput;
