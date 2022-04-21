e626671543b3b5b6fba5ad038838da4a04bae186
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-04-21 10:58:33 +0100

    Extract TabPanel.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import {useEffect} from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import {
   Button,
@@ -16,32 +14,7 @@ import {
 import Settings from "./components/Settings";
 import {Info} from "./components/Info";
 import {Assets} from "./components/Assets";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const {children, value, index, ...other} = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{p: 3}}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+import {TabPanel} from "./components/TabPanel";
 
 function a11yProps(index: number) {
   return {
---
+++ src/components/TabPanel.tsx
@@ -0,0 +1,29 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+export function TabPanel(props: TabPanelProps) {
+  const {children, value, index, ...other} = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{p: 3}}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
