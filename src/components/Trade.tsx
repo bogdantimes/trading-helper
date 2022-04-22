7d80ddd976e87a04e186bd524c32a6556d93a129
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-04-22 13:52:36 +0100

    Update trade card to new charts

--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -1,60 +1,88 @@
 import * as React from 'react';
+import {useEffect, useRef, useState} from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import {CardHeader} from "@mui/material";
 import {TradeMemo} from "../../apps-script/TradeMemo";
 import {Config} from "../../apps-script/Store";
-import {useState} from "react";
+import {createChart} from 'lightweight-charts';
 
 export default function Trade(props) {
   const tradeMemo: TradeMemo = props.data;
   const config: Config = props.config;
   const takeProfitPrice = tradeMemo.tradeResult.price * (1 + config.TakeProfit);
-  const [sellDisabled, setSellDisabled] = useState(false);
+
+  const chartContainerRef = useRef();
+  const chart = useRef(null);
+
+  useEffect(() => {
+    chart.current = createChart(chartContainerRef.current, {
+      width: 300,
+      height: 200,
+      timeScale: {visible: false}
+    });
+
+    chart.current.timeScale().setVisibleLogicalRange({from: 0.5, to: 1.5});
+
+    const mapFn = (v, i) => ({time: `2000-01-0${i + 1}`, value: v});
+
+    chart.current
+      .addLineSeries({color: "blue", lineWidth: 1})
+      .setData(tradeMemo.prices.map(mapFn));
+
+    chart.current
+      .addLineSeries({color: "red", lineWidth: 1})
+      .setData(new Array(tradeMemo.prices.length).fill(tradeMemo.stopLossPrice).map(mapFn));
+
+    chart.current
+      .addLineSeries({color: "green", lineWidth: 1})
+      .setData(new Array(tradeMemo.prices.length).fill(takeProfitPrice).map(mapFn));
+
+    chart.current
+      .addLineSeries({color: "gold", lineWidth: 1})
+      .setData(new Array(tradeMemo.prices.length).fill(tradeMemo.tradeResult.price).map(mapFn));
+
+  }, []);
+
+
+  const [isSelling, setIsSelling] = useState(false);
 
   function onSell() {
     if (confirm(`Are you sure you want to sell ${props.name}?`)) {
+      setIsSelling(true);
+      const handle = resp => {
+        alert(resp.toString());
+        setIsSelling(false);
+      };
       // @ts-ignore
-      google.script.run.withSuccessHandler((resp) => {
-        setSellDisabled(true);
-        alert(resp.toString())
-      }).sellCoin(props.name);
+      google.script.run.withSuccessHandler(handle).withFailureHandler(handle).sellCoin(props.name);
+    }
+  }
+
+  const [isBuying, setIsBuying] = useState(false);
+
+  function onBuyMore() {
+    if (confirm(`Are you sure you want to buy more ${props.name}?`)) {
+      setIsBuying(true);
+      const handle = resp => {
+        alert(resp.toString());
+        setIsBuying(false);
+      };
+      // @ts-ignore
+      google.script.run.withSuccessHandler(handle).withFailureHandler(handle).buyCoin(props.name);
     }
   }
 
   const lossPercent = (100 * (tradeMemo.maxLoss / tradeMemo.tradeResult.paid)).toFixed(2)
   const profitPercent = (100 * (tradeMemo.maxProfit / tradeMemo.tradeResult.paid)).toFixed(2)
 
-  // @ts-ignore
   return (
     <Card>
-      <CardHeader title={props.name}/>
-      <XYPlot xType="linear" width={300} height={200}>
-        <HorizontalGridLines/>
-        <XAxis hideTicks/>
-        <YAxis title="Price"/>
-        <LineSeries
-          color='blue'
-          data={tradeMemo.prices.map((y, x) => ({x, y}))}
-        />
-        <LineSeries
-          color='red'
-          data={new Array(tradeMemo.prices.length).fill(tradeMemo.stopLossPrice).map((y, x) => ({x, y}))}
-        />
-        <LineSeries
-          color='green'
-          data={new Array(tradeMemo.prices.length).fill(takeProfitPrice).map((y, x) => ({x, y}))}
-        />
-        <LineSeries
-          color='gold'
-          data={new Array(tradeMemo.prices.length).fill(tradeMemo.tradeResult.price).map((y, x) => ({x, y}))}
-        />
-      </XYPlot>
       <CardContent>
+        <Typography gutterBottom variant="h5" component="div">{props.name}</Typography>
+        <div ref={chartContainerRef} className="chart-container"/>
         <Typography variant="body2" color="text.secondary">
           <div>Total: {tradeMemo.tradeResult.paid.toFixed(2)}</div>
           <div>{tradeMemo.maxProfit > 0 ? "Profit" : "Loss"}: {tradeMemo.maxProfit.toFixed(2)} ({profitPercent}%)</div>
@@ -62,8 +90,8 @@ export default function Trade(props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" disabled={sellDisabled} onClick={onSell}>Sell</Button>
-        <Button size="small" disabled={true}>Buy More</Button>
+        <Button size="small" disabled={isSelling} onClick={onSell}>{isSelling ? 'Selling' : 'Sell'}</Button>
+        <Button size="small" disabled={isBuying} onClick={onBuyMore}>{isBuying ? 'Buying...' : 'Buy More'}</Button>
       </CardActions>
     </Card>
   );
