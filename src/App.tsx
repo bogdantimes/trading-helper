46043cbd9ace88feca6831bd783d6501697fa53d
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-05-07 21:22:21 +0100

    Revert

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,7 @@ export default function App() {
       {!fetchingData && !initialSetup &&
         <Box sx={{width: '100%'}}>
           <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-            <Tabs value={value} onChange={handleChange} centered variant={'scrollable'}>
+            <Tabs value={value} onChange={handleChange} centered>
               <Tab label="Assets" {...a11yProps(0)} />
               <Tab label="Settings" {...a11yProps(1)} />
               <Tab label="Info" {...a11yProps(2)} />
