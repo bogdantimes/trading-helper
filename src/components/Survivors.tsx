e6385c7faa4aa5cb96b7eb8830fb419c0d9cef28
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-05-12 17:21:15 +0100

    Add info alert on survivors tab

--- src/components/Survivors.tsx
+++ src/components/Survivors.tsx
@@ -21,8 +21,8 @@ export function Survivors() {
     <Box sx={{justifyContent: 'center', display: 'flex'}}>
       <Stack spacing={2}>
         <Alert sx={{width: 300}} severity={"info"}>
-          Score represents how many times a currency showed a price growth within last {CoinScore.PRICES_MAX_CAP}
-          measures, while 99% of the market was not moving up.
+          Score represents how many times a currency showed a price growth within
+          last {CoinScore.PRICES_MAX_CAP} measures, while 99% of the market was not moving up.
         </Alert>
         {!!survivors.length &&
           <List sx={{padding: 0, width: 300}}>
