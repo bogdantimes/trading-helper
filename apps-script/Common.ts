502100b3bb00e6db779afc26d923bd9558f6014c
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-05-13 15:12:13 +0100

    Adjust error print

--- apps-script/Common.ts
+++ apps-script/Common.ts
@@ -53,7 +53,7 @@ class Log {
 
   static print(): string {
     return `${this.alerts.length > 0 ? `${this.alerts.join('\n')}\n` : ''}
-${this.errLog.length > 0 ? `Errors:\n${this.errLog.map(e => `Message: ${e.message}\nStacktrace: ${e.stack}`).join('\n')}\n` : ''}
+${this.errLog.length > 0 ? `Errors:\n${this.errLog.map(e => `Stack: ${e.stack}`).join('\n')}\n` : ''}
 ${this.infoLog.length > 0 ? `Info:\n${this.infoLog.join('\n')}\n` : ''}
 ${this.debugLog.length > 0 ? `Debug:\n${this.debugLog.map(v => JSON.stringify(v)).join('\n\n')}` : ''}
 `
