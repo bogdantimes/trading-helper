07d9b21555aef10dcc30aca1de875583b3c31e07
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-05-15 18:02:05 +0100

    Info view fine tuning

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,9 +3,10 @@ import {useEffect} from "react";
 import {Stats} from "../../apps-script/Statistics";
 import {Box, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 import {FixedSizeList} from 'react-window';
+import {ArrowDropDown, ArrowDropUp} from "@mui/icons-material";
 
 export function Info() {
-  const [stats, setStats] = React.useState<Stats>({DailyProfit: {}, TotalProfit: NaN});
+  const [stats, setStats] = React.useState<Stats>(null);
 
   useEffect(() => {
     google.script.run.withSuccessHandler(setStats).getStatistics();
@@ -13,27 +14,33 @@ export function Info() {
 
   const rows = [];
 
-  rows.push({id: 1, timeFrame: 'Total', profit: stats.TotalProfit});
-  Object.keys(stats.DailyProfit)
-    .sort((a, b) => new Date(a) < new Date(b) ? 1 : -1)
-    .forEach((d, i) => {
-      rows.push({id: i + 2, timeFrame: d, profit: stats.DailyProfit[d]});
-    });
+  if (stats) {
+    rows.push({id: 1, timeFrame: 'Total', profit: stats.TotalProfit});
+    Object.keys(stats.DailyProfit)
+      .sort((a, b) => new Date(a) < new Date(b) ? 1 : -1)
+      .forEach((d, i) => {
+        rows.push({id: i + 2, timeFrame: d, profit: stats.DailyProfit[d]});
+      });
+  }
 
   return (
     <Box sx={{justifyContent: 'center', display: 'flex'}}>
       <FixedSizeList
-        width={300}
+        width={332}
         height={400}
-        itemSize={46}
+        itemSize={55}
         itemCount={rows.length}
         overscanCount={5}
       >
-        {({index, style}) =>
-          <ListItem style={style} key={index} component="div" disablePadding>
-            <ListItemAvatar>#{rows[index].id}</ListItemAvatar>
-            <ListItemText primary={rows[index].profit} secondary={rows[index].timeFrame}/>
-          </ListItem>
+        {
+          ({index, style}) => {
+            const up = rows[index].profit >= 0;
+            const icon = up ? <ArrowDropUp color={"success"}/> : <ArrowDropDown color={"error"}/>;
+            return <ListItem style={style} key={index} component="div">
+              <ListItemAvatar>{icon}</ListItemAvatar>
+              <ListItemText primary={rows[index].profit} secondary={rows[index].timeFrame}/>
+            </ListItem>;
+          }
         }
       </FixedSizeList>
     </Box>
