3aa3496476c360ddf6da1ba235bfe8ff8c375f08
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-06-14 09:15:47 +0100

    Mute some notifications

--- src/gas/traders/DefaultTrader.ts
+++ src/gas/traders/DefaultTrader.ts
@@ -91,8 +91,6 @@ export class DefaultTrader {
     if (priceDropped) {
       Log.alert(`ℹ️ ${symbol} will be bought again as price dropped sufficiently`)
       tm.setState(TradeState.BUY)
-    } else {
-      Log.info(`${symbol} price has not dropped sufficiently, skipping swing trade`)
     }
   }
 
@@ -108,7 +106,7 @@ export class DefaultTrader {
       Log.alert(`ℹ️ ${tm.getCoinName()} profit limit crossed up at ${tm.currentPrice}`)
       this.config.SellAtProfitLimit && tm.setState(TradeState.SELL)
     } else if (tm.entryPriceCrossedUp()) {
-      Log.alert(`ℹ️ ${tm.getCoinName()} entry price crossed up at ${tm.currentPrice}`)
+      // Log.alert(`ℹ️ ${tm.getCoinName()} entry price crossed up at ${tm.currentPrice}`)
     }
   }
 
