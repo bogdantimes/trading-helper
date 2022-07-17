ede3d9c4729c2a873b60bf89a9f8633a6d569b69
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-07-17 11:12:22 +0100

    Visualize channel state transitions

--- src/web/components/Channels.tsx
+++ src/web/components/Channels.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import { useEffect } from "react"
 import { Box, List, ListItem, ListItemAvatar, ListItemText, Stack, Typography } from "@mui/material"
-import { cardWidth, circularProgress } from "./Common"
-import { Config, f8, Key, PriceChannelsDataResponse } from "../../lib"
+import { cardWidth, circularProgress, growthIconMap } from "./Common"
+import { ChannelState, Config, f8, Key, PriceChannelsDataResponse, PriceMove } from "../../lib"
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material"
 
 export function Channels({ config }: { config: Config }) {
   const [priceChannelsData, setPriceChannelsData] = React.useState<PriceChannelsDataResponse>(null)
@@ -29,6 +30,12 @@ function list(data: PriceChannelsDataResponse, config: Config) {
   const keysSortedByDuration = Object.keys(data).sort(
     (a, b) => data[b][Key.DURATION] - data[a][Key.DURATION],
   )
+  const stateIcon = {
+    [ChannelState.NONE]: growthIconMap.get(PriceMove.NEUTRAL),
+    [ChannelState.TOP]: growthIconMap.get(PriceMove.UP),
+    [ChannelState.BOTTOM]: growthIconMap.get(PriceMove.DOWN),
+    [ChannelState.MIDDLE]: growthIconMap.get(PriceMove.NEUTRAL),
+  }
 
   return (
     <>
@@ -46,7 +53,14 @@ function list(data: PriceChannelsDataResponse, config: Config) {
             sx={{ padding: 0, marginTop: 0, width: cardWidth, overflow: `auto`, maxHeight: 440 }}
           >
             {keysSortedByDuration.map((coin, i) => {
-              const { [Key.DURATION]: duration, [Key.MIN]: min, [Key.MAX]: max } = data[coin]
+              const {
+                [Key.DURATION]: duration,
+                [Key.MIN]: min,
+                [Key.MAX]: max,
+                [Key.S0]: s0,
+                [Key.S1]: s1,
+                [Key.S2]: s2,
+              } = data[coin]
               const dataHint = `${duration}/${config.ChannelWindowMins} | ${f8(min)} | ${f8(max)}`
               return (
                 <ListItem
@@ -59,7 +73,12 @@ function list(data: PriceChannelsDataResponse, config: Config) {
                   <ListItemAvatar sx={{ minWidth: `48px` }}>#{i + 1}</ListItemAvatar>
                   <ListItemText
                     sx={{ margin: `3px 0 0 0` }}
-                    primary={coin + (duration > config.ChannelWindowMins ? ` ✅` : ``)}
+                    primary={
+                      <Typography sx={{ display: `flex`, alignItems: `center` }}>
+                        {coin + (duration > config.ChannelWindowMins ? ` ✅ ` : ``)}
+                        {stateIcon[s2]} {stateIcon[s1]} {stateIcon[s0]}
+                      </Typography>
+                    }
                     secondary={dataHint}
                   />
                 </ListItem>
