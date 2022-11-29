986a1ea54584d99c2f76446ae6134ca06883749c
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-11-29 12:42:37 +0000

    Fix error

--- src/gas/Binance.ts
+++ src/gas/Binance.ts
@@ -309,9 +309,10 @@ export class Binance implements IExchange {
   }
 
   getImbalance(symbol: ExchangeSymbol, limit: number): number {
-    const url = `https://api.binance.com/api/v3/depth?symbol=${symbol}&limit=${limit}`;
-    const resp = UrlFetchApp.fetch(url);
-    const data = JSON.parse(resp.getContentText());
+    const data = this.fetch(
+      () => `depth?symbol=${symbol}&limit=${limit}`,
+      this.defaultReqOpts
+    );
     const bidsVol: number = data.bids.reduce((s: number, b) => s + +b[1], 0);
     const asksVol: number = data.asks.reduce((s: number, a) => s + +a[1], 0);
     return (bidsVol - asksVol) / (bidsVol + asksVol);
