730a889f386da0da778d281c3137133d1c1da407
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2023-04-17 11:49:22 +0100

    API console

--- src/web/components/APIConsole.tsx
+++ src/web/components/APIConsole.tsx
@@ -4,7 +4,7 @@ import Terminal, {
   TerminalInput,
   TerminalOutput,
 } from "react-terminal-ui";
-import { Box, Dialog } from "@mui/material";
+import { Dialog } from "@mui/material";
 import { useTheme } from "@mui/system";
 import { ScriptApp } from "./Common";
 
@@ -37,77 +37,99 @@ export const APIConsole: React.FC<Props> = ({
   );
   const [prompt, setPrompt] = React.useState(`$`);
 
+  const handleResize = () => {
+    const terminalWidth = window.innerWidth * 0.8; // Set terminal width to 80% of the window width
+    const terminalHeight = window.innerHeight * 0.8; // Set terminal height to 80% of the window height
+    setTerminalDimensions({ width: terminalWidth, height: terminalHeight });
+  };
+
+  React.useEffect(() => {
+    handleResize();
+    window.addEventListener(`resize`, handleResize);
+    return () => {
+      window.removeEventListener(`resize`, handleResize);
+    };
+  }, []);
+
+  const [terminalDimensions, setTerminalDimensions] = React.useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
+
+  const onCommand = (command) => {
+    if (command.toLocaleLowerCase().trim() === `clear`) {
+      setTerminalOutput(``);
+      return;
+    }
+    setTerminalOutput(<TerminalInput>{command}</TerminalInput>);
+
+    const [cmd, ...args] = command.split(` `);
+
+    if (!ScriptApp?.withSuccessHandler(() => {})[cmd]) {
+      setTerminalOutput(
+        <>
+          <TerminalInput>{command}</TerminalInput>
+          <TerminalOutput>Unrecognized command</TerminalOutput>
+        </>
+      );
+      return;
+    }
+
+    setPrompt(`⏳`);
+    const spinner = setInterval(() => {
+      setPrompt((p) => (p === `⏳` ? `⌛` : `⏳`));
+    }, 1000);
+
+    try {
+      ScriptApp?.withSuccessHandler((resp) => {
+        setPrompt(`$`);
+        clearInterval(spinner);
+        setTerminalOutput(
+          <>
+            <TerminalInput>{command}</TerminalInput>
+            {resp.trim ? (
+              resp.split(`\n`).map((s, i) => {
+                return <TerminalOutput key={i}>{s}</TerminalOutput>;
+              })
+            ) : (
+              <TerminalOutput>{JSON.stringify(resp, null, 2)}</TerminalOutput>
+            )}
+          </>
+        );
+        reFetchState();
+      })
+        .withFailureHandler((resp) => {
+          setPrompt(`$`);
+          clearInterval(spinner);
+          setTerminalOutput(resp.message);
+        })
+        [cmd](...args);
+    } catch (e) {
+      clearInterval(spinner);
+    }
+  };
   return (
     <Dialog
+      fullWidth={true}
+      maxWidth={`md`}
       open={terminalOpen}
+      sx={{
+        [`.react-terminal-line`]: { whiteSpace: `pre-wrap` },
+      }}
       onClose={() => {
         setTerminalOpen(false);
       }}
     >
-      <Box width={600} height={400}>
-        <Terminal
-          name="API"
-          prompt={prompt}
-          height={`290px`}
-          colorMode={themeMode === `dark` ? ColorMode.Dark : ColorMode.Light}
-          onInput={(input) => {
-            if (input.toLocaleLowerCase().trim() === `clear`) {
-              setTerminalOutput(``);
-              return;
-            }
-            setTerminalOutput(<TerminalInput>{input}</TerminalInput>);
-
-            const [cmd, ...args] = input.split(` `);
-
-            if (!ScriptApp?.withSuccessHandler(() => {})[cmd]) {
-              setTerminalOutput(
-                <>
-                  <TerminalInput>{input}</TerminalInput>
-                  <TerminalOutput>Unrecognized command</TerminalOutput>
-                </>
-              );
-              return;
-            }
-
-            setPrompt(`⏳`);
-            const spinner = setInterval(() => {
-              setPrompt((p) => (p === `⏳` ? `⌛` : `⏳`));
-            }, 1000);
-
-            try {
-              ScriptApp?.withSuccessHandler((resp) => {
-                setPrompt(`$`);
-                clearInterval(spinner);
-                setTerminalOutput(
-                  <>
-                    <TerminalInput>{input}</TerminalInput>
-                    {resp.trim ? (
-                      resp.split(`\n`).map((s, i) => {
-                        return <TerminalOutput key={i}>{s}</TerminalOutput>;
-                      })
-                    ) : (
-                      <TerminalOutput>
-                        {JSON.stringify(resp, null, 2)}
-                      </TerminalOutput>
-                    )}
-                  </>
-                );
-                reFetchState();
-              })
-                .withFailureHandler((resp) => {
-                  setPrompt(`$`);
-                  clearInterval(spinner);
-                  setTerminalOutput(resp.message);
-                })
-                [cmd](...args);
-            } catch (e) {
-              clearInterval(spinner);
-            }
-          }}
-        >
-          {terminalOutput}
-        </Terminal>
-      </Box>
+      <Terminal
+        name="API"
+        prompt={prompt}
+        // Subtract some pixels to accommodate the terminal header and footer
+        height={`${terminalDimensions.height - 110}px`}
+        colorMode={themeMode === `dark` ? ColorMode.Dark : ColorMode.Light}
+        onInput={onCommand}
+      >
+        {terminalOutput}
+      </Terminal>
     </Dialog>
   );
 };
